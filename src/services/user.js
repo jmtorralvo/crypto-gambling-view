c4bb0331a3a760d730acbd42ed3ff5064c72b833
Josep Servat <jservatlorca@gmail.com>
2018-09-28 15:10:48 +0200

    add user bet request

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,19 @@
 const axios = require('axios');
 
-export const loginUser = (name) => 
+export const loginUser = (name) =>
   axios
    .post('http://hackathon.guidesmiths.com:4000/api/user', {
      name: name,
    })
+
+export const userBet = (name,currentBet,currentChange) =>
+axios({
+  method: 'get',
+  url: 'http://hackathon.guidesmiths.com:4000/api/nav/log',
+  data: {
+     gameId: "crypto-fighters",
+     userName: name,
+     bet: currentBet,
+     change: currentChange
+ }
+})
