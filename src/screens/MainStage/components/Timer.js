4f62566705672b1f0d5e495dbfb8b9764e0bd34d
Javier <javier.manzano22@gmail.com>
2018-09-28 13:30:46 +0200

    counter

--- src/screens/MainStage/components/Timer.js
+++ src/screens/MainStage/components/Timer.js
@@ -1,3 +1,65 @@
+import React, {Component} from 'react';
+
 import {
   Timer as TimerStyle
 } from '../styles';
+
+
+export default class Timer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: {
+        time: 0,
+        isOn: false,
+        start: 0
+      }
+    };
+    this.startTimer = this.startTimer.bind(this)
+  }
+
+  startTimer() {
+    this.setState({
+      timer: {
+        isOn: true,
+        time: 10,
+        start: Date.now(),
+      }
+    })
+    this.timer = setInterval(() => {
+      console.log('this.state.timer.time', this.state.timer.time)
+      if (this.state.timer.time === 1) {
+        this.setState({
+          timer: {
+            isOn: false,
+            time: 0,
+          }
+        });
+        this.props.onFinish && this.props.onFinish();
+      } else {
+        this.setState({
+          timer: {
+            isOn: true,
+            time: this.state.timer.time - 1,
+          }
+        })
+      }
+    }, 1000);
+  }
+
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.timer.isOn && <TimerStyle>{this.state.timer.time}</TimerStyle>}
+        { !this.state.timer.isOn && <TimerStyle>Time's finished</TimerStyle>}
+      </div>
+    )
+  }
+
+}
--- src/screens/MainStage/index.js
+++ src/screens/MainStage/index.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
 
 import Bet from './components/Bet';
+import Timer from './components/Timer';
 
-import {
-  Timer,
-} from './styles';
 
 
 const COUNTDOWN_START = 10;
@@ -15,60 +13,7 @@ class MainStage extends Component {
     this.state = {
       stage: '',
       bet: '',
-      timer: {
-        time: 0,
-        isOn: false,
-        start: 0
-      }
     };
-    this.startTimer = this.startTimer.bind(this)
-    this.stopTimer = this.stopTimer.bind(this)
-    this.resetTimer = this.resetTimer.bind(this)
-  }
-
-  startTimer() {
-    this.setState({
-      timer: {
-        isOn: true,
-        time: 10,
-        start: Date.now(),
-      }
-    })
-    this.timer = setInterval(() => {
-      console.log('this.state.timer.time', this.state.timer.time)
-      if (this.state.timer.time === 1) {
-        this.setState({
-          timer: {
-            isOn: false,
-            time: 0,
-          }
-        });
-      } else {
-        this.setState({
-          timer: {
-            isOn: true,
-            time: this.state.timer.time - 1,
-          }
-        })
-      }
-    }, 1000);
-  }
-
-  stopTimer() {
-    this.setState({
-      timer: {
-        isOn: false
-      }
-    })
-    clearInterval(this.timer);
-  }
-
-  resetTimer() {
-    this.setState({
-      timer: {
-        time: 0, isOn: false
-      }
-    })
   }
 
   onSelectBet(amount){
@@ -77,16 +22,11 @@ class MainStage extends Component {
     });
   }
 
-  componentDidMount() {
-    this.startTimer();
-  }
 
   render() {
     return (
       <div className="MainStage">
-        { this.state.timer.isOn && <Timer>{this.state.timer.time}</Timer>}
-        { !this.state.timer.isOn && <Timer>Time's finished</Timer>}
-        
+        <Timer />
         <div>
           <Bet onSelect={amount => this.onSelectBet(amount)}></Bet>
         </div>
