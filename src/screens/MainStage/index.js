7210fdc3cf61d933cc9a5ca97e08bcde282e39e4
jmtorralvo <thelastonealive@gmail.com>
2018-09-28 12:53:32 +0200

    Bet step

--- src/screens/MainStage/index.js
+++ src/screens/MainStage/index.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
 
+import Bet from './components/Bet';
+
 class MainStage extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      stage:'',
+      stage: '',
+      bet: ''
     };
   }
 
+  onSelectBet(amount){
+    this.setState({
+      bet: amount
+    });
+  }
 
 
   render() {
     return (
       <div className="MainStage">
         <div>
-          <h2>Main stage:</h2>
+          <Bet onSelect={amount => this.onSelectBet(amount)}></Bet>
         </div>
       </div>
     );
